04f3b21fd9378ba330046b8ab2f443b80ff8b363
noaione <noaione0809@gmail.com>
2024-01-23 15:58:06 +0700

    Bump assets to 0.1.1

--- scripts/download-assets.py
+++ scripts/download-assets.py
@@ -1,10 +1,9 @@
 import os
-from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
 
 CURRENT_DIR = Path(__file__).parent.parent.absolute()
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 os.chdir(CURRENT_DIR)
 
 ASSETS_PATH = f"https://github.com/noaione/cmx-ermitteln/releases/download/wasm-{VERSION}/ermitteln-wasm-pkg.zip"
