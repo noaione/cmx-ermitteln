129f8218667fb2a787dd1465d35436e69489a91e
noaione <noaione0809@gmail.com>
2024-01-23 12:23:01 +0700

    Try to use urllib instead of requests

--- scripts/download-assets.py
+++ scripts/download-assets.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
 
-import requests
+import urllib3
 
 CURRENT_DIR = Path(__file__).parent.parent.absolute()
 VERSION = "0.1.0"
@@ -10,10 +10,11 @@ VERSION = "0.1.0"
 ASSETS_PATH = f"https://github.com/noaione/cmx-ermitteln/releases/download/wasm-{VERSION}/ermitteln-wasm-pkg.zip"
 
 print(f"Downloading assets from {ASSETS_PATH}...")
-req = requests.get(ASSETS_PATH)
+http = urllib3.PoolManager()
+req = http.request("GET", ASSETS_PATH)
 
 # Open the file in read mode
-file_obj = BytesIO(req.content)
+file_obj = BytesIO(req.data)
 zip_file = ZipFile(file_obj)
 
 # Extract every files into ermitteln-wasm/pkg
