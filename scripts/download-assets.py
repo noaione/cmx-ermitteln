bd44bc87ec593317aa1e6e6ec2622678a547e175
noaione <noaione0809@gmail.com>
2024-01-23 12:24:43 +0700

    Use system wget to download assets

--- scripts/download-assets.py
+++ scripts/download-assets.py
@@ -1,22 +1,21 @@
+import os
 from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
 
-import urllib3
-
 CURRENT_DIR = Path(__file__).parent.parent.absolute()
 VERSION = "0.1.0"
+os.chdir(CURRENT_DIR)
 
 ASSETS_PATH = f"https://github.com/noaione/cmx-ermitteln/releases/download/wasm-{VERSION}/ermitteln-wasm-pkg.zip"
 
 print(f"Downloading assets from {ASSETS_PATH}...")
-http = urllib3.PoolManager()
-req = http.request("GET", ASSETS_PATH)
+# Use wget?
+os.system(f"wget {ASSETS_PATH} -O ermitteln-wasm-pkg.zip")
 
 # Open the file in read mode
-file_obj = BytesIO(req.data)
-zip_file = ZipFile(file_obj)
-
-# Extract every files into ermitteln-wasm/pkg
-print("Extracting files...")
-zip_file.extractall(path=CURRENT_DIR / "ermitteln-wasm" / "pkg")
+print("Opening zip file...")
+with ZipFile(CURRENT_DIR / "ermitteln-wasm-pkg.zip", "r") as zip_file:
+    # Extract every files into ermitteln-wasm/pkg
+    print("Extracting files...")
+    zip_file.extractall(path=CURRENT_DIR / "ermitteln-wasm" / "pkg")
