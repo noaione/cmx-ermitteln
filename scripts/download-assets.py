d8c6f913d20a471bf1a3135ff6805dc8bb4952f2
noaione <noaione0809@gmail.com>
2024-08-16 16:36:21 +0700

    chore: bump WASM to 0.1.4

--- scripts/download-assets.py
+++ scripts/download-assets.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 from pathlib import Path
 from zipfile import ZipFile
@@ -7,13 +8,26 @@ cf_pages = os.getenv("CF_PAGES")
 vercel = os.getenv("VERCEL")
 gh_runner = os.getenv(key="CI")
 
+VERSION_RE = re.compile(r"^version = \"(.*)\"$")
+
 # Check if we are in the CI
 if cf_pages is None and vercel is None and gh_runner is None:
     print("This script should only be run in CI")
     sys.exit(0)
 
-CURRENT_DIR = Path(__file__).parent.parent.absolute()
-VERSION = "0.1.3"
+
+def get_version(cargo_toml: Path) -> str:
+    read_lines = cargo_toml.read_text().splitlines()
+    for line in read_lines:
+        if match := VERSION_RE.match(line):
+            return match.group(1)
+    raise ValueError("Version not found in Cargo.toml")
+
+
+CURRENT_DIR = Path(__file__).resolve().parent.parent
+cargo_toml = CURRENT_DIR / "Cargo.toml"
+
+VERSION = get_version(cargo_toml)
 os.chdir(CURRENT_DIR)
 
 ASSETS_PATH = f"https://github.com/noaione/cmx-ermitteln/releases/download/wasm-{VERSION}/ermitteln-wasm-pkg.zip"
